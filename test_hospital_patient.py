c8e3d7e5da6ae3405733ea46a58336fe735a0010
Sean Del Castillo <seanbdcastillo@gmail.com>
2023-11-29 09:16:02 -0800

    added comments for clarity

--- test_hospital_patient.py
+++ test_hospital_patient.py
@@ -1,12 +1,14 @@
 import odoo 
 from odoo.tests import TransactionCase
 
+# tag to isolate test fixture when starting Odoo "--test-tags hospital"
 @odoo.tests.tagged("-standard", "hospital")
 class TestHospitalPatient(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super(TestHospitalPatient, cls).setUpClass()
 
+        # creating patient record for use in all fixture testcases
         cls.patient = cls.env["hospital.patient"].create({
             "name": "John Smith",
         })
